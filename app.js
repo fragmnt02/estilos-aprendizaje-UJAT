cdfb2afd5d71f909294900a20cd300720e6cfc2c
Pako <pakoconvans@gmail.com>
2017-08-24 13:22:44 -0500

    'ready'

--- app.js
+++ app.js
@@ -15,7 +15,6 @@ connection.connect(function(error){
 	}
 });
 var app= express();
-app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine','jade');
